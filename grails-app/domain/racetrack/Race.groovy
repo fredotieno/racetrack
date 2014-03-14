b80d173329fdb11a55e961d7490a2e69a4e6382d
fredotieno <fred.otieno@students.jkuat.ac.ke>
2014-03-14 11:42:57 +0300

    Set up integration and unit tests

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -1,18 +1,27 @@
 package racetrack
 
 class Race {
+    static constraints = {
+        name(blank:false, maxSize:50)
+	startDate(validator: {return(it > new Date())})
+	city()
+	state(inList:["GA", "NC", "SC", "VA"])
+	distance(range:0..10)
+	cost(min:0.0, max:100.0)
+	maxRunners(min:0, max:100000)
+    }
     String name
     Date startDate
     String city
     String state
     BigDecimal distance
     BigDecimal cost
-    Integer maxRunners = 100000
+    Integer maxRunners
 
-    static constraints = {
+    BigDecimal inMiles(){
+	return distance * 0.6214
     }
-
-    static mapping = {
-        sort "startDate"
+    static mappings = {
+	sort "startDate"
     }
 }
